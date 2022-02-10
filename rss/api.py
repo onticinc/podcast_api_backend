a82bf059609a83f3a25082cc5885b61673cc1070
onticinc <onticinc@gmail.com>
2022-02-10 16:02:46 -0700

    api.py

--- rss/api.py
+++ rss/api.py
@@ -3,7 +3,7 @@ import http.client
 conn = http.client.HTTPSConnection("api.simplecast.com")
 payload = ''
 headers = {}
-conn.request("GET", "/episodes/eyJhcGlfa2V5IjoiZjliNTUzZjg5MTEwYmIyOWNkMzZmOTQwYTFkMjZlYjkifQ==", payload, headers)
+conn.request("GET", "/episodes/8d60a36a-8830-4d05-af67-e6e1667a0741", payload, headers)
 res = conn.getresponse()
 data = res.read()
 print(data.decode("utf-8"))
